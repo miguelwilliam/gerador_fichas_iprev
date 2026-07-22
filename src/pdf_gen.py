db01617856e8105f0211721c5ab4b277c4d94b4a
Miguel William <mwemidio@gmail.com>
2026-07-22 13:27:29 -0300

    nova tabela e novo bd

--- src/pdf_gen.py
+++ src/pdf_gen.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 
 from reportlab.lib import colors
-from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import A4, landscape
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 
@@ -12,21 +12,37 @@ def gerarRelatorio(dados_json, caminho_pdf):
     with open(dados_json, 'r', encoding='utf-8') as arquivo:
         funcionarios = json.load(arquivo)
 
-    dados = [["Funcionário", "Salário", "Percentual", "Valor Auxílio"]]
+    dados = [["Matrícula", "Nome", "Salário\nEfetivo", "Comiss. /\nF. Grat.", "Total da\nRemuneração", "Remuneração\nde Contribuição", "Contribuição\n Prev. Própria"]]
+
+    #ADICIONAR OS VALORES PARA CADA FUNCIONÁRIO
 
     for funcionario in funcionarios:
-        salario = funcionario['salario']
-        percentual = funcionario['percentualAuxilios']
-        valor_auxilio = salario * percentual / 100
+        matricula = funcionario['MATRICULA']
+        nome = funcionario['NOME']
+        instituicao = funcionario['INSTITUICAO']
+        salario_efetivo = funcionario['SALARIO_EFETIVO']
+        gratificacao = funcionario['GRATIFICACAO']
+        total_remuneracao = salario_efetivo + gratificacao
+        percentual_segurados = 14/100
+        percentual_patronal = 15.83/100
+        segurados = percentual_segurados*salario_efetivo
+        patronal = percentual_patronal*salario_efetivo
+
+        dados.append(['', instituicao, '', '', '', '', ''])
 
         dados.append([
-            funcionario['nome'],
-            f"R$ {salario:,.2f}",
-            f"{percentual:.1f}%",
-            f"R$ {valor_auxilio:,.2f}"
+            matricula,
+            nome,
+            f"{salario_efetivo:,.2f}",
+            f"{gratificacao:,.2f}",
+            f"{total_remuneracao:,.2f}",
+            f"{salario_efetivo:,.2f}",
+            f"{segurados:,.2f}"
         ])
 
-    pdf = SimpleDocTemplate(str(caminho_pdf), pagesize=A4)
+    # ADICIONAR VALORES DE SOMA NO FINAL DA TABELA
+
+    pdf = SimpleDocTemplate(str(caminho_pdf), pagesize=landscape(A4))
 
     tabela = Table(dados)
 
